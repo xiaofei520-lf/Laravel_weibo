c178eb59536b3f2981e9399b11f8d3bf0a1023a6
小菜鸟阿飞 <2686558013@qq.com>
2022-03-28 17:20:20 +0800

    用户显示页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\User;
 class UsersController extends Controller
 {
+    //创建用户
     public function create()
     {
         return view('users.create');
     }
+    //显示个人信息
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
+
 }
