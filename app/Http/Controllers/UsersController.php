5e0959dd141ac959b374f5c34124cd4501551898
小菜鸟阿飞 <2686558013@qq.com>
2022-03-29 09:32:26 +0800

    用户退出登录

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -35,4 +35,9 @@ class UsersController extends Controller
         return redirect()->route('users.show',[$user]);
     }
 
+    public function destory(){
+        Auth::logout();
+        session()->flash('success','您已成功退出');
+        return redirect('login');
+    }
 }
