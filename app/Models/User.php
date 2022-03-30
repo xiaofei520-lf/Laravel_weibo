1dd27f3fe97325c2940ebd6084d1426da1cf067c
小菜鸟阿飞 <2686558013@qq.com>
2022-03-30 15:35:08 +0800

    首页微博列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -61,6 +61,10 @@ class User extends Authenticatable
     {
         return $this->hasMany(Status::class);
     }
+    //获取当前用户所有的微博
+    public function feed(){
+        return $this->statuses()->orderBy('created_at','desc');
+    }
 
 
 }
